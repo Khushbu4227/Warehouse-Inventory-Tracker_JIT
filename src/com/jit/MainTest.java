d74f0b303dd430a1ef7b364301783a52dd351a9c
Khushbu4227 <mukatikhushboo47@gamil.com>
2025-10-19 16:15:43 +0530

    Add menu system

--- src/com/jit/MainTest.java
+++ src/com/jit/MainTest.java
@@ -1,44 +1,89 @@
 package com.jit;
-
+import java.util.Scanner;
 
 public class MainTest {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         AlertService alert = new AlertService();
 
-        //  Create multiple warehouses
         Warehouse warehouse1 = new Warehouse("Indore");
         Warehouse warehouse2 = new Warehouse("Pune");
 
         warehouse1.registerObserver(alert);
         warehouse2.registerObserver(alert);
 
-        // Load data from files (if exists)
         warehouse1.loadInventoryFromFile("warehouse1.txt");
         warehouse2.loadInventoryFromFile("warehouse2.txt");
 
-        // Add products
-        Product laptop = new Product("P01", "Laptop", 10, 5);
-        Product mobile = new Product("P02", "Mobile", 5, 2);
+        System.out.println("\n WAREHOUSE INVENTORY TRACKER MENU ");
 
-        warehouse1.addProduct(laptop);
-        warehouse2.addProduct(mobile);
+        boolean running = true;
+        while (running) {
+            System.out.println("\n1. Add Product");
+            System.out.println("2. Receive Shipment");
+            System.out.println("3. Fulfill Order");
+            System.out.println("4. Show Inventory");
+            System.out.println("5. Save & Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine(); 
 
-        // Shipments
-        warehouse1.receiveShipment("P01", 5);
-        warehouse2.receiveShipment("P02", 2);
+            Warehouse current;
+            System.out.print("Select Warehouse :-    1 = Indore, 2 = Pune:  ");
+            int wChoice = sc.nextInt();
+            sc.nextLine();
+            current = (wChoice == 1) ? warehouse1 : warehouse2;
 
-        // Orders
-        warehouse1.fulfillOrder("P01", 12); // trigger alert
-        warehouse2.fulfillOrder("P02", 5);  // trigger alert
+            switch (choice) {
+            // case 1 for add product
+                case 1:
+                    System.out.print("Enter Product ID: ");
+                    String id = sc.nextLine().trim();
+                    System.out.print("Enter Product Name: ");
+                    String name = sc.nextLine().trim();
+                    System.out.print("Enter Quantity: ");
+                    int qty = sc.nextInt();
+                    System.out.print("Enter Threshold: ");
+                    int th = sc.nextInt();
+                    sc.nextLine();
+                    current.addProduct(new Product(id, name, qty, th));
+                    break;
+            //case 2 for Recieve Shipment
+                case 2:
+                    System.out.print("Enter Product ID to receive shipment: ");
+                    String shipId = sc.nextLine().trim();
+                    System.out.print("Enter Quantity received: ");
+                    int sQty = sc.nextInt();
+                    sc.nextLine();
+                    current.receiveShipment(shipId, sQty);
+                    break;
+            // fulfil oreder
+                case 3:
+                    System.out.print("Enter Product ID to fulfill order: ");
+                    String orderId = sc.nextLine().trim();
+                    System.out.print("Enter Quantity ordered: ");
+                    int oQty = sc.nextInt();
+                    sc.nextLine();
+                    current.fulfillOrder(orderId, oQty);
+                    break;
+                // show  all products
+                case 4:
+                    warehouse1.showAllProducts();
+                    warehouse2.showAllProducts();
+                    break;
+                 // Save Product in files
+                case 5:
+                    warehouse1.saveInventoryToFile("warehouse1.txt");
+                    warehouse2.saveInventoryToFile("warehouse2.txt");
+                    System.out.println(" Inventory saved. Exiting...");
+                    running = false;
+                    break;
 
-        // Show products
-        warehouse1.showAllProducts();
-        warehouse2.showAllProducts();
+                default:
+                    System.out.println(" Invalid choice. Try again.");
+            }
+        }
 
-        // Save to file
-        warehouse1.saveInventoryToFile("warehouse1.txt");
-        warehouse2.saveInventoryToFile("warehouse2.txt");
-
-        System.out.println("\n  All operations completed successfully!");
+        sc.close();
     }
 }
